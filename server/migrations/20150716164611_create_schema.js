865a7501fd7516d0d06d6a8e07cccda5115697f1
Will Roberts <willroberts3@protonmail.com>
2022-09-30 08:36:14 -0400

    Comment bluebird import

--- server/migrations/20150716164611_create_schema.js
+++ server/migrations/20150716164611_create_schema.js
@@ -1,6 +1,5 @@
 // Migration date convention: moment().utc().format('YYYYMMDDHHMMSS')
-
-
+// const Promise = require('bluebird');
 exports.up = function(knex, Promise) {
 	return Promise.all([
 	  	knex.schema.createTable('games', function (table) {
